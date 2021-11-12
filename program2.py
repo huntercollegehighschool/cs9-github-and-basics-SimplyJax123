5e544b6014dffe57c5c8625c08e3eafda4eb624e
SimplyJax123 <94186657+SimplyJax123@users.noreply.github.com>
2021-11-12 10:46:14 -0500

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,12 +1,13 @@
 import time
 
-print("Hello, you. Let me add two numbers for you.")
+print("Hello, you. Let me add three numbers for you.")
 
 time.sleep(0.5)
 
 num1 = input("Enter a number, then press enter: ")
 num2 = input("Enter another number, then press enter: ")
-print("The sum is", num1 + num2)
+num3 = input("Enter a 3rd number, then press enter: ")
+print("The sum is", num1 + num2 + num3)
 print("Did I add that correctly?")
 
 time.sleep(1)
@@ -17,6 +18,7 @@ time.sleep(0.5)
 
 num1 = int(input("Enter a number, then press enter: "))
 num2 = int(input("Enter another number, then press enter: "))
+num3 = input("Enter a 3rd number, then press enter: ")
 print("The sum is", num1 + num2)
 
 time.sleep(0.5)
